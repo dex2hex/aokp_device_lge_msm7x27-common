b993a1cd9ab9ac4523402e7eec3b25a7fa3a70fc
williamcharles <parvezshk786@gmail.com>
2012-12-01 22:09:34 +0530

    Remove Offline Charging and Add Patch for ARMv6 OMX

--- device.mk
+++ device.mk
@@ -128,10 +128,7 @@ PRODUCT_PACKAGES += \
     hciconfig \
     hwaddrs
 
-# Offmode charging
-PRODUCT_PACKAGES += \
-    charger \
-    charger_res_images
+
 
 
 PRODUCT_TAGS += dalvik.gc.type-precise
